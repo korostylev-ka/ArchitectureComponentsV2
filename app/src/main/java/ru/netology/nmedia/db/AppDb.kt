4eb6a20c25b4ec4e1d67ca808714f301498819c7
k.korostylev <k.korostylev@gmail.com>
2023-02-11 17:16:16 +0500

    ArchitectureComponentsV2

---
+++ app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -0,0 +1,16 @@
+package ru.netology.nmedia.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.netology.nmedia.dao.PostDao
+import ru.netology.nmedia.dao.PostRemoteKeyDao
+import ru.netology.nmedia.entity.PostEntity
+import ru.netology.nmedia.entity.PostRemoteKeyEntity
+
+//сообщаем Room о новой таблице
+@Database(entities = [PostEntity::class, PostRemoteKeyEntity::class], version = 1, exportSchema = false)
+abstract class AppDb : RoomDatabase() {
+    abstract fun postDao(): PostDao
+    //и новых запросах
+    abstract fun postRemoteKeyDao(): PostRemoteKeyDao
+}
